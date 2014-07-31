8c09d24e178b36e2b89870b0441c5602dade9b12
wilk666 <andre@cartomatic.pl>
2014-07-31 17:52:32 +0200

    Unsuccessful gridster config
    Still cannot find solution providing gridster d&d/resize with responsive
    design(aka fluid interface)

--- Scripts/UiManager/GadgetManager.js
+++ Scripts/UiManager/GadgetManager.js
@@ -11,21 +11,17 @@
 
     var me = this;
 
+
     this.gridsterOptions = {
-        //max_size_x: true,
-        //max_size_y: true,
-        autogrow_cols: true, //allow for infinite x axis grow
-        avoid_overlapped_widgets: true,
+        avoid_overlapped_widgets: false,
+        autogrow_cols: true,
         widget_base_dimensions: [100, 100],
         widget_margins: [2, 2],
         helper: 'clone',
-        max_cols: 2,
-        //extra_rows: 1,
-        //extra_cols: 1,
         draggable: {
             start: function (e, ui, $widget) {
 
-                gadgetManager.hideAll();
+                me.hideAll();
 
                 //var frameId = ui.$player.contents().find("iframe")[0].id;
                 //var iframe = $('#' + frameId);
@@ -37,7 +33,7 @@
             //    //log.innerHTML = 'DRAG offset: ' + ui.pointer.diff_top +' '+ ui.pointer.diff_left + "<br >" + log.innerHTML;
             //},
             stop: function (e, ui) {
-                gadgetManager.showAll();
+                me.showAll();
                 //var frameId = ui.$player.contents().find("iframe")[0].id;
                 //var iframe = $('#' + frameId);
                 //iframe.show();
@@ -48,10 +44,11 @@
             enabled: true,
             //max_size: [4, 3],
             min_size: [1, 1],
-            max_cols: 1,
+            max_size: [Infinity, Infinity],
+            //max_cols: 2,
             start: function (e, ui, $widget) {
 
-                gadgetManager.hideAll();
+                me.hideAll();
 
                 //var frameId = $widget.contents().find("iframe")[0].id;
                 //var iframe = $('#' + frameId);
@@ -59,7 +56,7 @@
                 //console.warn('resize start?', e, ui, $widget);
             },
             stop: function (e, ui, $widget) {
-                
+
                 var newHeight = this.resize_coords.data.height;
                 var newWidth = this.resize_coords.data.width;
 
@@ -84,10 +81,89 @@
 
                 //iframe.show();
 
-                gadgetManager.showAll();
+                me.showAll();
             }
         }
-    };
+    }
+
+    //this.gridsterOptions = {
+    //    //max_size_x: true,
+    //    //max_size_y: true,
+    //    autogrow_cols: true, //allow for infinite x axis grow
+    //    avoid_overlapped_widgets: true,
+    //    widget_base_dimensions: [100, 100],
+    //    widget_margins: [2, 2],
+    //    helper: 'clone',
+    //    max_cols: 2,
+    //    axes: ['x', 'y'],
+    //    //extra_rows: 1,
+    //    //extra_cols: 1,
+    //    draggable: {
+    //        start: function (e, ui, $widget) {
+
+    //            gadgetManager.hideAll();
+
+    //            //var frameId = ui.$player.contents().find("iframe")[0].id;
+    //            //var iframe = $('#' + frameId);
+    //            //iframe.hide();
+    //            //log.innerHTML = 'START position: ' + ui.position.top +' '+ ui.position.left + "<br >" + log.innerHTML;
+    //        },
+    //        //drag: function (e, ui) {
+    //        //    console.warn('drag drag', arguments);
+    //        //    //log.innerHTML = 'DRAG offset: ' + ui.pointer.diff_top +' '+ ui.pointer.diff_left + "<br >" + log.innerHTML;
+    //        //},
+    //        stop: function (e, ui) {
+    //            gadgetManager.showAll();
+    //            //var frameId = ui.$player.contents().find("iframe")[0].id;
+    //            //var iframe = $('#' + frameId);
+    //            //iframe.show();
+    //            //log.innerHTML = 'STOP position: ' + ui.position.top +' '+ ui.position.left + "<br >" + log.innerHTML;
+    //        }
+    //    },
+    //    resize: {
+    //        enabled: true,
+    //        //max_size: [4, 3],
+    //        min_size: [1, 1],
+    //        max_cols: 2,
+    //        start: function (e, ui, $widget) {
+
+    //            gadgetManager.hideAll();
+
+    //            //var frameId = $widget.contents().find("iframe")[0].id;
+    //            //var iframe = $('#' + frameId);
+    //            //iframe.hide();
+    //            //console.warn('resize start?', e, ui, $widget);
+    //        },
+    //        stop: function (e, ui, $widget) {
+                
+    //            var newHeight = this.resize_coords.data.height;
+    //            var newWidth = this.resize_coords.data.width;
+
+    //            // get title
+    //            var titleHeight = 0;
+    //            $widget.find('.portlet-header')
+    //              .each(function () {
+    //                  titleHeight = $(this).height();
+    //              });
+
+    //            var footerHeight = 0;
+    //            $widget.find('.gs-resize-handle')
+    //            .each(function () {
+    //                footerHeight = $(this).height();
+    //            });
+
+    //            var afterResizeHeight = newHeight - titleHeight - footerHeight;
+
+    //            var frameId = $widget.contents().find("iframe")[0].id;
+    //            var iframe = $('#' + frameId);
+    //            iframe.height(afterResizeHeight);
+
+    //            //iframe.show();
+
+    //            gadgetManager.showAll();
+    //        }
+    //    }
+    //};
 
 }
 
@@ -99,7 +175,7 @@ GnxGadgetManager.prototype.addGadget = function (gadgetUrl) {
     var divId = this.generateUUID();
     var widget = ['<li id=' + divId + '></li>', sizeFactor, sizeFactor];
 
-    var gster = gridster.add_widget.apply(gridster, widget)
+    gridster.add_widget.apply(gridster, widget)
 
     var w = this.gridsterOptions.widget_base_dimensions[0] * sizeFactor;
     var h = this.gridsterOptions.widget_base_dimensions[1] * sizeFactor;
@@ -114,7 +190,21 @@ GnxGadgetManager.prototype.addGadget = function (gadgetUrl) {
     window.gnxPreloadAndAddGadget(gadgetUrl, divId, w, h);
 
     gridster.set_dom_grid_width();
+    gridster.set_dom_grid_height();
 
+    console.warn('gridster', gridster);
+
+    //var gridster = $(".gridster ul").gridster().data('gridster');
+    //gridster.options.min_cols = 1; // Not necessarily required because of the following size changes, but I did it for clarity
+    ////gridster.options.widget_base_dimensions = [100, 100];
+    ////gridster.options.min_widget_width = 100;
+
+    //// This section was for existing widgets. Apparently the code for drawing the droppable zones is based on the data stored in the widgets at creation time
+    //for (var i = 0; i < gridster.$widgets.length; i++) {
+    //    gridster.resize_widget($(gridster.$widgets[i]), 1, 1);
+    //}
+
+    //gridster.generate_grid_and_stylesheet();
     //gadgetManager
 
 }
